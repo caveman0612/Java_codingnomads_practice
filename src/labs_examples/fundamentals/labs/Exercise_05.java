04da470f7ca4df8f97ad0ef7c9638671e2cf8e89
caveman0612 <75807458+caveman0612@users.noreply.github.com>
2020-12-30 14:19:50 -0800

    finished exercise 05

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,21 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int x = 3;
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
 
+        boolean S = false;
+
+
         // please concatenate str & str2 and set the result to a new String variable below
+        String new_str = str  + " " + str2;
+        System.out.println(new_str);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        System.out.println(str.replace('l', 'k'));
 
     }
 
